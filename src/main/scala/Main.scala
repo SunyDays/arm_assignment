2e8bfb932df59cf0fbefd6a6b6ed0c9dd41c3e70
Roman Lakeev <sunnyddayss@gmail.com>
2019-07-07 20:35:08 +0300

    Main: open and parse ELF file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,10 @@
 package arm_assignment
 
+import java.io.File
+import net.fornwall.jelf._
+
 object Main {
   def main(args: Array[String]): Unit = {
-    }
+    val elfFile = ElfFile.fromFile(new File(args(0)))
   }
 }
