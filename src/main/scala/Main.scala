7b22f055bd82c42e61f8f156b1267929f330ad61
Roman Lakeev <sunnyddayss@gmail.com>
2019-07-13 11:51:49 +0300

    Main: print serialized ELF data

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,9 +2,12 @@ package arm_assignment
 
 import java.io.File
 import net.fornwall.jelf._
+import spray.json._
+import arm_assignment.ElfFileJsonProtocol._
 
 object Main {
   def main(args: Array[String]): Unit = {
     val elfFile = ElfFile.fromFile(new File(args(0)))
+    println(elfFile.toJson.prettyPrint)
   }
 }
